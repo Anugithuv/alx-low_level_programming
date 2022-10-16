b77bb32f2bd03bfd30eb50c484ab72e676a84f32
Anugithuv <anuoluwapoadekanmbi192@gmail.com>
2022-10-16 14:29:32 +0100

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd>
+/**
+ * main - Entry point
+ * Description: print all number
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
